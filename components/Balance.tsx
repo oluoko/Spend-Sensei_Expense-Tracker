73c1524baf2178d93ec1f2f66d9dbefc5b03078a
oluoko <brianotieno586@gmail.com>
2024-10-07 15:40:21 +0300

    Expense Tracker(Next.js): getUserBalance Action

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,8 +1,11 @@
-const Balance = () => {
+import getUserBalance from "@/app/actions/getUserBalance";
+
+const Balance = async () => {
+  const { balance } = await getUserBalance();
   return (
     <>
       <h4>Your Balance</h4>
-      <h1 className="text-4xl font-bold">Ksh, 500</h1>
+      <h1 className="text-4xl font-bold">Ksh, {balance}</h1>
     </>
   );
 };

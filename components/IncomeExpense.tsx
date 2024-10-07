9111a26b4f0d1b3bd34338c4cea505f9421b2bd8
oluoko <brianotieno586@gmail.com>
2024-10-07 18:01:03 +0300

    Expense Tracker(Next.js): addCommas() and twoDP in IncomeExpense.tsx

--- components/IncomeExpense.tsx
+++ components/IncomeExpense.tsx
@@ -1,5 +1,6 @@
 import exp from "constants";
 import getIncomeExpense from "@/app/actions/getIncomeExpense";
+import { addCommas, twoDP } from "@/lib/utils";
 
 const InomeExpense = async () => {
   const { income, expense } = await getIncomeExpense();
@@ -7,11 +8,11 @@ const InomeExpense = async () => {
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">Ksh {income}</p>
+        <p className="money plus">Ksh {addCommas(twoDP(income))}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">Ksh {expense}</p>
+        <p className="money minus">Ksh {addCommas(twoDP(expense))}</p>
       </div>
     </div>
   );

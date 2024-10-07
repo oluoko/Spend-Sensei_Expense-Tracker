cf2f7125467f73188a0423d257189d636451e3fe
oluoko <brianotieno586@gmail.com>
2024-10-07 14:58:40 +0300

    Expense Tracker(Next.js): Get & Validate User

--- app/actions/addTransaction.ts
+++ app/actions/addTransaction.ts
@@ -1,4 +1,5 @@
 "use server";
+import { auth } from "@clerk/nextjs/server";
 
 interface TransactionData {
   text: string;
@@ -21,6 +22,14 @@ async function addTransaction(formData: FormData): Promise<TransactionResult> {
 
   const text: string = textValue.toString(); // Ensure text is a string
   const amount: number = parseFloat(amountValue.toString()); // Parse amount as number
+
+  // Get logged in user
+  const { userId } = auth();
+
+  // Check for user
+  if (!userId) {
+    return { error: "User not found" };
+  }
 
   const transactionData: TransactionData = {
     text,

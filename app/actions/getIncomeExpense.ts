3374978864bcbcc902bbf7dc2af7d7d4fb4ea228
oluoko <brianotieno586@gmail.com>
2024-10-07 16:59:16 +0300

    Expense Tracker(Next.js): getIncomeExpense Action

---
+++ app/actions/getIncomeExpense.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+
+async function getIncomeExpense(): Promise<{
+  income?: number;
+  expense?: number;
+  error?: string;
+}> {
+  const { userId } = auth();
+
+  if (!userId) {
+    return { error: "User not found" };
+  }
+
+  try {
+    const transactions = await db.transaction.findMany({
+      where: {
+        userId,
+      },
+    });
+
+    const amounts = transactions.map((transactions) => transactions.amount);
+
+    const income = amounts
+      .filter((amount) => amount > 0)
+      .reduce((sum, amount) => sum + amount, 0);
+
+    const expense = amounts
+      .filter((amount) => amount < 0)
+      .reduce((sum, amount) => sum + amount, 0);
+
+    return { income, expense: Math.abs(expense) };
+  } catch (error) {
+    return { error: "Database error" };
+  }
+}
+
+export default getIncomeExpense;
--- components/IncomeExpense.tsx
+++ components/IncomeExpense.tsx
@@ -1,15 +1,17 @@
 import exp from "constants";
+import getIncomeExpense from "@/app/actions/getIncomeExpense";
 
-const InomeExpense = () => {
+const InomeExpense = async () => {
+  const { income, expense } = await getIncomeExpense();
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">Ksh 700</p>
+        <p className="money plus">Ksh {income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minux">Ksh 200</p>
+        <p className="money minus">Ksh {expense}</p>
       </div>
     </div>
   );

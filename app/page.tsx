34befbfcace9b3a78e3f9f95e58899bc1c2d516a
oluoko <brianotieno586@gmail.com>
2024-10-07 15:26:49 +0300

    Expense Tracker(Next.js): Balance Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AddTransaction from "@/components/AddTransaction";
 import Guest from "@/components/Guest";
 import { currentUser } from "@clerk/nextjs/server";
+import Balance from "@/components/Balance";
 
 const HomePage = async () => {
   const user = await currentUser();
@@ -11,16 +12,17 @@ const HomePage = async () => {
 
   return (
     <main>
-      <h1 className="py-6 text-xl">
+      <h2 className="py-6 text-xl">
         Welcome,{" "}
         <span className="text-2xl font-extrabold font-sans">
           {" "}
           {user.firstName}
         </span>
         , to Spend Sensei
-      </h1>
+      </h2>
 
       <p className="text-lg font-semibold mb-8">Master your money moves!</p>
+      <Balance />
       <AddTransaction />
     </main>
   );
---
+++ components/Balance.tsx
@@ -0,0 +1,10 @@
+const Balance = () => {
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1 className="text-4xl font-bold">Ksh, 500</h1>
+    </>
+  );
+};
+
+export default Balance;

32ff13b28ab0f2103403d453e0251736f1f1b7b1
oluoko <brianotieno586@gmail.com>
2024-10-07 13:14:04 +0300

    Expense Tracker(Next.js): Clerk Authentication Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
@@ -16,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={roboto.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={roboto.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }

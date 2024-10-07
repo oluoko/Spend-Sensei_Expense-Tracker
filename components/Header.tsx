b0ac59ce33175326e7597f2f01ffd1a1cf9a71bc
oluoko <brianotieno586@gmail.com>
2024-10-08 00:45:05 +0300

    Expense Tracker(Next.js): Updated styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = async () => {
 
   return (
     <nav className="navbar h-20 bg-[#505760] ">
-      <div className="navbar-container h-full w-full md:w-4/5 ">
+      <div className="navbar-container h-full w-full md:w-4/5 my-4 ">
         <h2>Spend Sensei</h2>
         <div className="">
           <SignedOut>

df4884f07bf499344991aeeaa41a22be8eec5b4d
oluoko <brianotieno586@gmail.com>
2024-10-07 17:35:12 +0300

    Expense Tracker(Next.js): Delete Button

--- components/ TransactionItem.tsx
+++ components/ TransactionItem.tsx
@@ -4,12 +4,28 @@ import { toast } from "react-toastify";
 
 const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
+
+  const handleDeleteTransaction = async (transactionId: string) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this transaction?"
+    );
+
+    if (!confirmed) {
+      return;
+    }
+
+    await deleteTransaction(transactionId);
+
+    toast.success("Transaction deleted successfully");
+  };
+
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text}{" "}
       <span>
         {sign} Ksh {addCommas(twoDP(Math.abs(transaction.amount)))}
       </span>
+      <button onClick={() => handleDeleteTransaction(transaction.id)}>x</button>
     </li>
   );
 };

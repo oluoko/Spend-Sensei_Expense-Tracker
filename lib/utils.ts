8c4528326a3350caf4a3a4d5fc70d7990e2d0299
oluoko <brianotieno586@gmail.com>
2024-10-07 18:28:30 +0300

    Expense Tracker(Next.js): Updated utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,3 @@
-// Function to add commas only to the left side of the decimal point
-export function addCommas(x: number | string): string {
-  const [integerPart, decimalPart] = x.toString().split(".");
-  return decimalPart
-    ? `${integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}.${decimalPart}`
-    : integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-// Function to format the number to two decimal places and return it as a string
-export function twoDP(x: number): string {
-  return x.toFixed(2);
+export function addCommas(x: number): string {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }

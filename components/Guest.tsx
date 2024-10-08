9fa0d4c6b6ceb9a22b67268939fbd6c84b46af38
oluoko <brianotieno586@gmail.com>
2024-10-08 15:13:39 +0300

    Expense Tracker(Next.js): Updated styling

--- components/Guest.tsx
+++ components/Guest.tsx
@@ -2,15 +2,18 @@ import { SignInButton } from "@clerk/nextjs";
 
 const Guest = () => {
   return (
-    <div className="guest w-full h-[60vh] md:h-[80vh]  flex flex-col justify-center items-center gap-3 md:gap-10">
+    <div className="guest w-full h-[60vh] md:h-[80vh]  flex flex-col justify-center items-center gap-5 md:gap-10">
       <h1 className="text-3xl md:text-6xl font-extrabold">
         Welcome to Spend Sensei
       </h1>
-      <p>Master your money moves!</p>
+      <p className="text-2xl md:text-4xl">Master your money moves!</p>
 
-      <p>Please sign in to manage your transactions </p>
-
-      <SignInButton />
+      <p className="text-2xl md:text-4xl">
+        Please sign in to manage your transactions{" "}
+      </p>
+      <div className="text-2xl md:text-4xl w-full">
+        <SignInButton />
+      </div>
     </div>
   );
 };

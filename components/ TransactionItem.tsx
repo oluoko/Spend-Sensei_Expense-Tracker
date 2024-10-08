c461ca6084e2245c656b94bac14b2f6d798c1c28
oluoko <brianotieno586@gmail.com>
2024-10-08 18:34:47 +0300

    Expense Tracker(Next.js): Updated styling

--- components/ TransactionItem.tsx
+++ components/ TransactionItem.tsx
@@ -25,23 +25,25 @@ const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
 
   return (
     <li
-      className={`grid grid-cols-2 md:grid-cols-4 gap-4 rounded-lg md:rounded-sm p-3 md:p-6 my-3 md:my-5 md:gap-10 w-full ${
+      className={`rounded-lg md:rounded-sm p-3 md:p-6 my-3 md:my-5 w-full ${
         transaction.amount < 0 ? "minus" : "plus"
       }`}
     >
-      <span className="w-2/6 h-[77px] md:h-[70px] overflow-hidden hover:h-full hover:overflow-x-hidden hover:overflow-auto custom-gradient relative ">
-        {transaction.text}
-      </span>
-      <span className="w-1/6 item-amount">
-        {sign} Ksh {addCommas(Math.abs(transaction.amount))}
-      </span>
-      <span className="w-2/6">
-        <span>{formatDate(new Date(transaction.createdAt), "date")}</span>
-      </span>
+      <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-10 ">
+        <span className=" h-[77px] md:h-[70px] overflow-hidden hover:h-full hover:overflow-x-hidden hover:overflow-auto custom-gradient relative ">
+          {transaction.text}
+        </span>
+        <span className="item-amount">
+          {sign} Ksh {addCommas(Math.abs(transaction.amount))}
+        </span>
+        <span className="">
+          <span>{formatDate(new Date(transaction.createdAt), "date")}</span>
+        </span>
 
-      <span className="w-1/6">
-        <span>{formatDate(new Date(transaction.createdAt), "time")}</span>
-      </span>
+        <span className="">
+          <span>{formatDate(new Date(transaction.createdAt), "time")}</span>
+        </span>
+      </div>
       <button
         onClick={() => handleDeleteTransaction(transaction.id)}
         className="delete-btn"

d543c427ee850e6ae100e055f2262b9a8fb37381
Brian Otieno <101041804+oluoko@users.noreply.github.com>
2024-10-09 18:22:00 +0300

    Update Welcome.tsx

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -6,10 +6,11 @@ const Welcome = ({ name }: WelcomeProps) => {
   return (
     <div className="flex flex-col justify-center items-center gap-5 md:gap-10">
       <h1 className="py-6 text-3xl md:text-5xl font-extrabold">
-        Welcome to Spend Sensei,{" "}
+    
         <span className="text-4xl md:text-6xl font-extrabold font-sans">
-          {name ? name : "deshi"}!
+          {name ? name : "Deshi"}, 
         </span>
+ welcome to Spend Sensei!
       </h1>
       <p className="text-2xl md:text-4xl">
         Take charge of your finances like a master.

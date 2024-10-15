88c65303771e2e086b6db80bf223a3ea55cfa99f
oluoko <brianotieno586@gmail.com>
2024-10-15 07:44:03 +0300

    Expense Tracker(Next.js): TransactionItem Styling

--- components/ TransactionItem.tsx
+++ components/ TransactionItem.tsx
@@ -30,7 +30,7 @@ const TransactionItem = ({ transaction }: { transaction: Transaction }) => {
       }`}
     >
       <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-1 md:gap-10 bg-slate-300 md:bg-transparent rounded-lg">
-        <span className=" h-[77px] md:h-[70px] overflow-hidden hover:h-full hover:overflow-x-hidden hover:overflow-auto custom-gradient relative grid md:flex flex-col justify-center items-center p-2 md:p-0 bg-[#fff] md:bg-transparent">
+        <span className=" h-[77px] md:h-[70px] overflow-hidden hover:h-full hover:overflow-x-hidden hover:overflow-y-auto custom-gradient relative p-2 md:p-0 bg-[#fff] md:bg-transparent ">
           {transaction.text}
         </span>
         <span className="item-amount grid md:flex flex-col justify-center items-center p-2 md:p-0 bg-[#fff] md:bg-transparent">
